fb7048060686bd9c724226a120f44e3773637760
Alsu <dxdy10@mail.ru>
2025-07-26 09:20:01 +0500

    4 type": "module

--- tasks/4/main.js
+++ tasks/4/main.js
@@ -4,7 +4,7 @@ import * as astring from "astring";
 import { parse } from "acorn";
 
 // read file with source code
-const source = fs.readFileSync("./src/entry.js", "utf-8");
+const source = fs.readFileSync("./entry.js", "utf-8");
 
 // get ast from source code
 const ast = parse(source, { ecmaVersion: 2020, sourceType: "module" });
@@ -14,7 +14,6 @@ transformer(ast);
 
 // convert ast to source code
 const result = astring.generate(ast);
-console.log("🚀 ~ result:", result);
 
 // write source code to file
 fs.writeFileSync("./result.js", result);
